67b495fdd45a13cfe9a2f3ecbba36f75b58270c5
Roman <alonzofroman@gmail.com>
2022-06-02 15:46:32 -0700

    Added view models to invoice for testing purposes

--- gamestore-invoicing/src/main/java/com/company/gamestoreinvoicing/controller/GameStoreControllerExceptionHandler.java
+++ gamestore-invoicing/src/main/java/com/company/gamestoreinvoicing/controller/GameStoreControllerExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.trilogyed.gamestore.controller;
+package com.company.gamestoreinvoicing.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
